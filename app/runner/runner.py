5f0a5638691baaa7df1a3af122226558258cb594
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2022-12-14 04:02:52 +0300

    artifacts & cleanup

--- app/runner/runner.py
+++ app/runner/runner.py
@@ -1,23 +1,27 @@
 import os
+import errno
+import shutil
 from os import path
 
 from ..database import SessionLocal
-from ..constants import RUNNER_CHECKOUTER_TAG, RUNNER_CLEANUP_TAG, RUNNER_TMP_DIR, RUNNER_LOGS_DIR
+from ..constants import RUNNER_CHECKOUTER_TAG, RUNNER_CLEANUP_TAG, RUNNER_TMP_DIR, RUNNER_LOGS_DIR, RUNNER_ARTIFACTS_DIR
 from ..schemas.stage import StageInternal, StageStatus
 from ..crud.stage import set_stage_status
 from .docker_client import docker_client
 
 def run_worker(stage: StageInternal, run_finished):
     run_dir = path.join(os.getcwd(), RUNNER_TMP_DIR, str(stage.run_id))
-    log_path = path.join(os.getcwd(), RUNNER_LOGS_DIR, str(stage.run_id))
+    artifacts_dir = path.join(os.getcwd(), RUNNER_ARTIFACTS_DIR, str(stage.run_id))
+    logs_dir = path.join(os.getcwd(), RUNNER_LOGS_DIR, str(stage.run_id))
 
     if stage.image_tag == RUNNER_CLEANUP_TAG:
-        # todo
+        shutil.rmtree(run_dir)
         return
 
     if stage.image_tag == RUNNER_CHECKOUTER_TAG:
-        os.makedirs(log_path)
         os.makedirs(run_dir)
+        os.makedirs(logs_dir)
+        os.makedirs(artifacts_dir)
 
     container = docker_client.containers.run(
         stage.image_tag,
@@ -26,7 +30,7 @@ def run_worker(stage: StageInternal, run_finished):
         environment=stage.env_vars
     )
 
-    with open(path.join(log_path, f'{stage.name}.log'), 'wb') as log:
+    with open(path.join(logs_dir, f'{stage.name}.log'), 'wb') as log:
         for log_line in container.logs(stream=True, stdout=True, stderr=True):
             log.write(log_line)
             log.flush()
@@ -36,6 +40,15 @@ def run_worker(stage: StageInternal, run_finished):
 
     with SessionLocal() as db:
         if status['StatusCode'] == 0:
+            if stage.artifacts:
+                for artifact_path in stage.artifacts:
+                    try:
+                        shutil.copytree(path.join(run_dir, artifact_path), path.join(artifacts_dir, artifact_path))
+                    except OSError as exc:
+                        if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+                            shutil.copy(path.join(run_dir, artifact_path), path.join(artifacts_dir, artifact_path))
+                        else: raise
+
             set_stage_status(db, status=StageStatus.Success, for_stage_id=stage.id)
             set_stage_status(db, status=StageStatus.Ready, for_stage_id=stage.next_stage)
         else:
